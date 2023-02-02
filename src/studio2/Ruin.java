0b193d00423e4b765f43e584a8196355d784223b
kaiyuky <kaiyu@wustl.edu>
2023-02-02 12:28:09 -0600

    P1 finished

--- src/studio2/Ruin.java
+++ src/studio2/Ruin.java
@@ -21,10 +21,11 @@ public class Ruin {
 		int totalSimulations = in.nextInt();
 		
 		
-		int currentAmount = startAmount;
+		int losses = 0;
 		for (int i = 0; i < totalSimulations; i++) {
+			int currentAmount = startAmount;
 			int count = 0;
-			while (currentAmount > 0 && currentAmount < winLimit) {
+			while (currentAmount > 0 && (currentAmount < winLimit)) {
 				double prob = Math.random();
 				if(prob <=  winChance) {
 					currentAmount = currentAmount + 1;
@@ -33,18 +34,30 @@ public class Ruin {
 					currentAmount = currentAmount - 1;
 				}
 				count = count + 1;
-				if (currentAmount == 0) {
-					System.out.println("Similation " + (i+1) + ":" + count + " LOSE");
-				}
-				if (currentAmount == winLimit)
-					System.out.println("Similation " + (i+1) +":" + + count + " Win");
-				}
-				
-			
-		}
-		
-		
 
+		}
+			if (currentAmount == 0) {
+				System.out.println("Similation " + (i+1) + ":" + count + " LOSE");
+				losses = losses + 1;
+			}
+			if (currentAmount == winLimit)
+				System.out.println("Similation " + (i+1) +":" + + count + " WIN");
+			}
+		double ruinRate = losses / 500.0;
+		System.out.println("Losses: " + losses + "Simulations: " + totalSimulations);
+		System.out.println("Ruin Rate from Simulation: " + ruinRate);
+		
+		
+		double expectedRuin;
+		double alpha;
+		alpha = (1 - winChance) * 1.0 / winChance;
+		if (winChance == .5) {
+			expectedRuin = 1 - 1.0 * (startAmount) / winLimit;
+		}
+		else {
+			expectedRuin = 
+	(Math.pow(alpha, 1.0 * startAmount) - Math.pow(alpha, 1.0 * winLimit)) / (1 - Math.pow(alpha, winLimit*1.0));
+		}
 	}
 
 }
